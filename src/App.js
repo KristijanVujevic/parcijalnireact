313d83c640233aee3b682ac910e4032523c5f7d2
KristijanV <kvuje60@gmail.com>
2023-12-07 14:48:21 +0100

    added theme

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, createContext } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import UserProfile from "./Components/UserProfile";
 import Forma from "./Components/Forma";
+import Tema from "./Components/Tema";
+import "./App.css"; // Import the CSS file
+
+export const Theme = createContext({ theme: "light", setTheme: () => {} });
 
 function App() {
+  const [theme, setTheme] = useState("light");
   const [selectedUsername, setSelectedUsername] = useState("");
 
   const handleUsernameSubmit = (username) => {
     setSelectedUsername(username);
   };
 
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
   return (
-    <div className="App">
-      <h1 style={{ textAlign: "center" }}>Parcijalni Ispit React</h1>
-      <Forma onUsernameSubmit={handleUsernameSubmit} />
-      {selectedUsername && <UserProfile username={selectedUsername} />}
+    <div className={`App ${theme}`}>
+      <Theme.Provider value={{ theme, setTheme }}>
+        <Tema />
+        <h1 style={{ textAlign: "center" }}>Parcijalni Ispit React</h1>
+
+        <Forma onUsernameSubmit={handleUsernameSubmit} />
+        {selectedUsername && <UserProfile username={selectedUsername} />}
+      </Theme.Provider>
     </div>
   );
 }
---
+++ src/Components/Tema.jsx
@@ -0,0 +1,31 @@
+import React, { useContext } from "react";
+import { Theme } from "../App";
+
+const Tema = () => {
+  // Destructure the array obtained from useContext
+  const { theme, setTheme } = useContext(Theme);
+
+  return (
+    <div className={`${theme}`}>
+      <label className="switch">
+        <input
+          type="checkbox"
+          checked={theme === "dark"}
+          onChange={(e) => {
+            setTheme(e.target.checked ? "dark" : "light");
+          }}
+        />
+        <span className="slider round"></span>
+        <div className={`${theme}`}>
+          {theme === "dark" ? (
+            <p>Switch to light mode</p>
+          ) : (
+            <p>Switch to dark mode</p>
+          )}
+        </div>
+      </label>
+    </div>
+  );
+};
+
+export default Tema;
